ab5d12081a00a10a3843f0c395947411df08befa
linhlbh <linhlbh@ymese.com>
2021-03-11 13:30:11 +0700

    nit

--- utils/request.js
+++ utils/request.js
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://localhost:3000', // url = base url + request url
+  baseURL: process.env.APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000, // request timeout
 });
